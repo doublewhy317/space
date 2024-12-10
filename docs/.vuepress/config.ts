90b0ce1aae6c5baef0d39a8cd13bd5f9ebf38682
Micky <52744666+doublewhy317@users.noreply.github.com>
2024-12-10 19:23:58 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/space/',
   lang: 'zh-CN',
   title: 'Micky`s Space',
   description: 'Micky',
